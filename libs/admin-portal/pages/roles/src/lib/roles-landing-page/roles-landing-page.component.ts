a5cb023b9f68f0e46a726a8126527c49ce968d69
Owen Kelvin <otienoowenkelvin@gmail.com>
2024-12-09 15:04:37 +0300

    roles pages

--- libs/admin-portal/pages/roles/src/lib/roles-landing-page/roles-landing-page.component.ts
+++ libs/admin-portal/pages/roles/src/lib/roles-landing-page/roles-landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
   IonCol,
   IonGrid,
@@ -9,10 +9,8 @@ import {
   IonIcon,
 } from '@ionic/angular/standalone';
 import { RolesStore } from '@lpg-manager/roles-store';
-import { CdkTableModule, DataSource } from '@angular/cdk/table';
-import { IGetRolesQuery } from '@lpg-manager/roles-store';
-import { toObservable } from '@angular/core/rxjs-interop';
-import { Observable } from 'rxjs';
+import { CdkTableModule } from '@angular/cdk/table';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'lpg-roles-page',
@@ -26,6 +24,7 @@ import { Observable } from 'rxjs';
     IonButton,
     IonIcon,
     CdkTableModule,
+    TitleCasePipe,
   ],
   template: `
     <ion-grid>
@@ -37,23 +36,27 @@ import { Observable } from 'rxjs';
                 <table cdk-table [dataSource]="roles()" class="roles-table">
                   <ng-container cdkColumnDef="name">
                     <th cdk-header-cell *cdkHeaderCellDef>Name</th>
-                    <td cdk-cell *cdkCellDef="let role">{{role.name}}</td>
+                    <td cdk-cell *cdkCellDef="let role">
+                      {{ role.name | titlecase }}
+                    </td>
                   </ng-container>
 
                   <ng-container cdkColumnDef="actions">
                     <th cdk-header-cell *cdkHeaderCellDef>Actions</th>
                     <td cdk-cell *cdkCellDef="let role">
-                      <ion-button fill="clear" size="small">
-                        <ion-icon name="create" slot="icon-only"></ion-icon>
+                      <ion-button shape="round" fill="clear" size="small">
+                        <ion-icon name="pen-to-square" slot="icon-only"></ion-icon>
                       </ion-button>
-                      <ion-button fill="clear" size="small" color="danger">
-                        <ion-icon name="trash" slot="icon-only"></ion-icon>
+                      <ion-button shape="round" fill="clear" size="small" color="danger">
+                        <ion-icon name="trash-can" slot="icon-only"></ion-icon>
                       </ion-button>
                     </td>
                   </ng-container>
-
                   <tr cdk-header-row *cdkHeaderRowDef="displayedColumns"></tr>
-                  <tr cdk-row *cdkRowDef="let row; columns: displayedColumns"></tr>
+                  <tr
+                    cdk-row
+                    *cdkRowDef="let row; columns: displayedColumns"
+                  ></tr>
                 </table>
               </div>
             </ion-card-content>
@@ -63,30 +66,7 @@ import { Observable } from 'rxjs';
     </ion-grid>
   `,
   styles: `
-    .table-container {
-      width: 100%;
-      overflow-x: auto;
-    }
 
-    .roles-table {
-      width: 100%;
-      border-collapse: collapse;
-    }
-
-    .roles-table th, .roles-table td {
-      padding: 1rem;
-      text-align: left;
-      border-bottom: 1px solid var(--ion-color-light);
-    }
-
-    .roles-table th {
-      font-weight: bold;
-      background-color: var(--ion-color-light);
-    }
-
-    .roles-table tr:hover {
-      background-color: var(--ion-color-light-shade);
-    }
   `,
   providers: [RolesStore],
 })
@@ -94,5 +74,5 @@ export default class RolesLandingPageComponent {
   rolesStore = inject(RolesStore);
   displayedColumns = ['name', 'actions'];
 
-  roles = this.rolesStore.items
+  roles = this.rolesStore.items;
 }
