074b6dec0115c234301f9114813aeaa7c7b87a26
Owen Kelvin <otienoowenkelvin@gmail.com>
2024-12-24 11:12:43 +0300

    catalogues

---
+++ libs/backend/db/src/lib/migrations/20241130062062-create-cart-catalogues.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cart_catalogues', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      cartId: {
+        field: 'cart_id',
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'carts',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      catalogueId: {
+        field: 'catalogue_id',
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'catalogues',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      quantity: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      createdAt: {
+        field: 'created_at',
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        field: 'updated_at',
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      deletedAt: {
+        field: 'deleted_at',
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+    });
+
+    await queryInterface.addIndex('cart_catalogues', ['cart_id', 'catalogue_id']);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('cart_catalogues');
+  }
+};
