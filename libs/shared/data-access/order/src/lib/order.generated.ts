4cbaf24dc18d94c395636365315211d8242bc2ac
Owen Kelvin <otienoowenkelvin@gmail.com>
2024-12-29 18:43:06 +0300

    fixes

--- libs/shared/data-access/order/src/lib/order.generated.ts
+++ libs/shared/data-access/order/src/lib/order.generated.ts
@@ -3,18 +3,91 @@ import * as Types from '@lpg-manager/types';
 import { gql } from 'apollo-angular';
 import { Injectable } from '@angular/core';
 import * as Apollo from 'apollo-angular';
+export type ICreateOrderMutationVariables = Types.Exact<{
+  params: Types.ICreateOrderInput;
+}>;
+
+
+export type ICreateOrderMutation = { createOrder: { message: string, data: { id: string } } };
+
+export type IGetOrderByIdQueryVariables = Types.Exact<{
+  id: Types.Scalars['UUID']['input'];
+}>;
+
+
+export type IGetOrderByIdQuery = { order?: { id: string } | null };
+
 export type IGetOrdersQueryVariables = Types.Exact<{
   query?: Types.InputMaybe<Types.IQueryParams>;
 }>;
 
 
-export type IGetOrdersQuery = { orders: { items?: Array<{ id: string } | null> | null, meta?: { totalItems: number } | null } };
+export type IGetOrdersQuery = { orders: { items?: Array<{ id: string, items: Array<{ catalogue: { name: string } } | null> } | null> | null, meta?: { totalItems: number } | null } };
 
+export type IDeleteOrderByIdMutationVariables = Types.Exact<{
+  id: Types.Scalars['UUID']['input'];
+}>;
+
+
+export type IDeleteOrderByIdMutation = { deleteOrder?: { message: string } | null };
+
+export type IUpdateOrderMutationVariables = Types.Exact<{
+  id: Types.Scalars['UUID']['input'];
+  params: Types.IUpdateOrderInput;
+}>;
+
+
+export type IUpdateOrderMutation = { updateOrder: { message: string, data: { id: string } } };
+
+export const CreateOrderDocument = gql`
+    mutation CreateOrder($params: CreateOrderInput!) {
+  createOrder(params: $params) {
+    message
+    data {
+      id
+    }
+  }
+}
+    `;
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class ICreateOrderGQL extends Apollo.Mutation<ICreateOrderMutation, ICreateOrderMutationVariables> {
+    override document = CreateOrderDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+export const GetOrderByIdDocument = gql`
+    query GetOrderById($id: UUID!) {
+  order(id: $id) {
+    id
+  }
+}
+    `;
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class IGetOrderByIdGQL extends Apollo.Query<IGetOrderByIdQuery, IGetOrderByIdQueryVariables> {
+    override document = GetOrderByIdDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
 export const GetOrdersDocument = gql`
     query GetOrders($query: QueryParams) {
   orders(query: $query) {
     items {
       id
+      items {
+        catalogue {
+          name
+        }
+      }
     }
     meta {
       totalItems
@@ -28,6 +101,45 @@ export const GetOrdersDocument = gql`
   })
   export class IGetOrdersGQL extends Apollo.Query<IGetOrdersQuery, IGetOrdersQueryVariables> {
     override document = GetOrdersDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+export const DeleteOrderByIdDocument = gql`
+    mutation DeleteOrderById($id: UUID!) {
+  deleteOrder(id: $id) {
+    message
+  }
+}
+    `;
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class IDeleteOrderByIdGQL extends Apollo.Mutation<IDeleteOrderByIdMutation, IDeleteOrderByIdMutationVariables> {
+    override document = DeleteOrderByIdDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+export const UpdateOrderDocument = gql`
+    mutation UpdateOrder($id: UUID!, $params: UpdateOrderInput!) {
+  updateOrder(id: $id, params: $params) {
+    message
+    data {
+      id
+    }
+  }
+}
+    `;
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class IUpdateOrderGQL extends Apollo.Mutation<IUpdateOrderMutation, IUpdateOrderMutationVariables> {
+    override document = UpdateOrderDocument;
     
     constructor(apollo: Apollo.Apollo) {
       super(apollo);
