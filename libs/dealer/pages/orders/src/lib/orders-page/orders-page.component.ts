b8c56d81b7676f97357fc6e451e35700afb99ad8
Owen Kelvin <otienoowenkelvin@gmail.com>
2024-12-30 08:22:09 +0300

    fixes

--- libs/dealer/pages/orders/src/lib/orders-page/orders-page.component.ts
+++ libs/dealer/pages/orders/src/lib/orders-page/orders-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, effect, inject, untracked } from '@angular/core';
 import { OrderStore } from '@lpg-manager/order-store';
 import {
   IonList,
@@ -12,6 +12,8 @@ import {
   IonItemDivider,
 } from '@ionic/angular/standalone';
 import { DatePipe, CurrencyPipe, JsonPipe } from '@angular/common';
+import { IQueryOperatorEnum } from '@lpg-manager/types';
+import { AuthStore } from '@lpg-manager/auth-store';
 
 @Component({
   selector: 'lpg-orders',
@@ -30,21 +32,41 @@ import { DatePipe, CurrencyPipe, JsonPipe } from '@angular/common';
     DatePipe,
     CurrencyPipe,
     IonItemDivider,
-    JsonPipe,
   ],
   providers: [OrderStore],
 })
 export default class OrdersPageComponent {
   #orderStore = inject(OrderStore);
-
+  #authStore = inject(AuthStore);
   orders = computed(() => this.#orderStore.searchedItemsEntities() || []);
   ordersDisplayed = computed(() =>
     this.orders().map((order) => ({
       ...order,
       color: this.getStatusColor(order.status),
-      varColor: `var(--ion-color-${this.getStatusColor(order.status)})`
+      varColor: `var(--ion-color-${this.getStatusColor(order.status)})`,
+      varColorLight: `rgba(var(--ion-color-${this.getStatusColor(
+        order.status
+      )}-rgb), 0.05)`,
     }))
   );
+  activeStationId = computed(() => this.#authStore.activeStation()?.id);
+  activeStationChangeEffect = effect(() => {
+    const activeStationId = this.activeStationId();
+    untracked(() => {
+      if (activeStationId) {
+        this.#orderStore.setFilters([
+          {
+            field: 'stationId',
+            operator: IQueryOperatorEnum.Equals,
+            value: activeStationId,
+            values: [],
+          },
+        ]);
+      }
+    });
+  });
+
+  constructor() {}
 
   getStatusColor(status: string): string {
     switch (status) {
