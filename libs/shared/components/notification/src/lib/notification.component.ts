6bdb38698ffc943450906620f4556bc949239233
Owen Kelvin <otienoowenkelvin@gmail.com>
2024-12-30 16:32:45 +0300

    notifications

---
+++ libs/shared/components/notification/src/lib/notification.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { IonButton, IonIcon, IonItem, IonLabel, IonList, IonPopover } from '@ionic/angular/standalone';
+import { NotificationStore } from '@lpg-manager/notification-store';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'lpg-notification-bell',
+  imports: [
+    IonIcon,
+    IonButton,
+    IonList,
+    IonPopover,
+    IonItem,
+    IonLabel,
+    JsonPipe,
+  ],
+  templateUrl: './notification.component.html',
+  styleUrl: './notification.component.css',
+})
+export class NotificationBellComponent {
+  #notificationStore = inject(NotificationStore);
+  notifications = this.#notificationStore.searchedItemsEntities;
+}
