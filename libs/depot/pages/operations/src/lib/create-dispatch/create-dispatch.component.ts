9a810eca5469a45ddb70a0d8d9a04bdf58918f1e
Owen Kelvin <otienoowenkelvin@gmail.com>
2025-01-01 23:13:43 +0300

    UI

--- libs/depot/pages/operations/src/lib/create-dispatch/create-dispatch.component.ts
+++ libs/depot/pages/operations/src/lib/create-dispatch/create-dispatch.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, effect, inject } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import {
   IonContent,
@@ -10,9 +17,10 @@ import {
   IonItemDivider,
   IonRow,
   IonCol,
+  IonPopover,
 } from '@ionic/angular/standalone';
 import { SearchableSelectComponent } from '@lpg-manager/searchable-select';
-import { OrderStore } from '@lpg-manager/order-store';
+import { IGetOrdersQuery, OrderStore } from '@lpg-manager/order-store';
 import { Router } from '@angular/router';
 import {
   TransporterStore,
@@ -22,7 +30,7 @@ import { DriverStore, IGetDriversQuery } from '@lpg-manager/driver-store';
 import { IGetVehiclesQuery, VehicleStore } from '@lpg-manager/vehicle-store';
 import { DispatchStore } from '@lpg-manager/dispatch-store';
 import { PaginatedResource } from '@lpg-manager/data-table';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, JsonPipe } from '@angular/common';
 import { IQueryOperatorEnum, ISelectCategory } from '@lpg-manager/types';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
@@ -38,9 +46,14 @@ type ITransporterItem = NonNullable<
   NonNullable<IGetTransportersQuery['transporters']['items']>[number]
 >;
 
+type IOrderItem = NonNullable<
+  NonNullable<IGetOrdersQuery['orders']['items']>[number]
+>;
+
 @Component({
   selector: 'lpg-create-dispatch',
   templateUrl: './create-dispatch.component.html',
+  styleUrl: './create-dispatch.component.scss',
   standalone: true,
   imports: [
     ReactiveFormsModule,
@@ -55,6 +68,8 @@ type ITransporterItem = NonNullable<
     CurrencyPipe,
     IonRow,
     IonCol,
+    IonPopover,
+    JsonPipe,
   ],
   providers: [
     TransporterStore,
@@ -65,6 +80,9 @@ type ITransporterItem = NonNullable<
   ],
 })
 export default class CreateDispatchComponent {
+  itemsPopoverOpen = signal(false);
+  itemsPopover = viewChild.required(IonPopover);
+  activeOrder = signal<IOrderItem | undefined>(undefined);
   #fb = inject(FormBuilder);
   #router = inject(Router);
   #dispatchStore = inject(DispatchStore);
@@ -132,6 +150,14 @@ export default class CreateDispatchComponent {
       });
   }
 
+  presentPopover($event: Event, order: IOrderItem) {
+    this.activeOrder.set(order);
+    $event.preventDefault();
+    const itemsPopover = this.itemsPopover();
+    itemsPopover.event = $event;
+    this.itemsPopoverOpen.set(true);
+  }
+
   onOrderSelect(event: any, order: any) {
     if (event.detail.checked) {
       this.selectedOrders.push(order.id);
@@ -143,7 +169,7 @@ export default class CreateDispatchComponent {
   driverValueLabelFormatter = (item: IDriverItem) =>
     `${item.user.firstName} ${item.user.lastName}`;
 
-  vehicleValueLabelFormatter= (item: IVehicleItem) =>
+  vehicleValueLabelFormatter = (item: IVehicleItem) =>
     `${item.registrationNumber}`;
 
   async createDispatch() {
