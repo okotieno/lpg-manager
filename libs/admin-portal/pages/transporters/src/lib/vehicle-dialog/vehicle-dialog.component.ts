eeb5148880e69efe6dcc9ddc3b2344604c1a0043
Owen Kelvin <otienoowenkelvin@gmail.com>
2025-01-01 12:17:08 +0300

    confirmation

--- libs/admin-portal/pages/transporters/src/lib/vehicle-dialog/vehicle-dialog.component.ts
+++ libs/admin-portal/pages/transporters/src/lib/vehicle-dialog/vehicle-dialog.component.ts
@@ -3,14 +3,14 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import {
   IonButton,
   IonButtons,
-  IonContent,
+  IonContent, IonFooter,
   IonHeader,
   IonIcon,
   IonInput,
-  IonItem,
+  IonItem, IonRow,
   IonTitle,
   IonToolbar,
-  ModalController,
+  ModalController
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -27,63 +27,10 @@ import {
     IonContent,
     IonItem,
     IonInput,
+    IonFooter,
+    IonRow,
   ],
-  template: `
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Add Vehicle</ion-title>
-        <ion-buttons slot="end">
-          <ion-button color="danger" fill="clear" shape="round" (click)="dismiss()">
-            <ion-icon slot="icon-only" name="circle-xmark"></ion-icon>
-          </ion-button>
-        </ion-buttons>
-      </ion-toolbar>
-    </ion-header>
-
-    <ion-content class="ion-padding">
-      <form [formGroup]="vehicleForm" (ngSubmit)="onSubmit()">
-        <ion-item lines="none">
-          <ion-input
-            label="Registration Number"
-            labelPlacement="stacked"
-            formControlName="registrationNumber"
-            placeholder="Enter registration number"
-            errorText="Registration number is required"
-          ></ion-input>
-        </ion-item>
-
-        <ion-item lines="none">
-          <ion-input
-            type="number"
-            label="Capacity"
-            labelPlacement="stacked"
-            formControlName="capacity"
-            placeholder="Enter capacity"
-            errorText="Capacity is required"
-          ></ion-input>
-        </ion-item>
-
-        <ion-item lines="none">
-          <ion-input
-            label="Vehicle Type"
-            labelPlacement="stacked"
-            formControlName="type"
-            placeholder="Enter vehicle type"
-            errorText="Vehicle type is required"
-          ></ion-input>
-        </ion-item>
-
-        <ion-button
-          type="submit"
-          expand="block"
-          class="ion-margin-top"
-          [disabled]="!vehicleForm.valid"
-        >
-          Add Vehicle
-        </ion-button>
-      </form>
-    </ion-content>
-  `,
+  templateUrl: 'vehicle-dialog.component.html',
 })
 export class VehicleDialogComponent {
   #modalCtrl = inject(ModalController);
