872479d5ad76c53dc6b3f8687f1f8752670e283d
Owen Kelvin <otienoowenkelvin@gmail.com>
2025-01-24 09:22:48 +0300

    build android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.depot.lpgmanager.app',
   appName: 'LPG-Manager',
-  webDir: 'dist/apps/depot/browser'
+  webDir: 'dist/apps/depot/browser',
+  server: {
+    androidScheme: 'http'
+  }
 };
 
 export default config;
