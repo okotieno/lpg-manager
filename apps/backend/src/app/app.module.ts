0b934a137d14cdd6dd2c784ba1d0a8e304e851de
Owen Kelvin <53997274+OwenKelvin@users.noreply.github.com>
2025-07-22 15:06:24 +0300

    minor updates

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -21,10 +21,10 @@ import { NotificationServiceModule } from '@lpg-manager/notification-service';
 import { UserServiceModule } from '@lpg-manager/user-service';
 import { NotificationModule } from '@lpg-manager/notification';
 import { PubSubProviderModule } from '@lpg-manager/util';
-import { VehicleModule } from '../../../../libs/backend/vehicle/src/lib/vehicle.module';
-import { TransporterModule } from '../../../../libs/backend/transporter/src/lib/transporter.module';
+import { VehicleModule } from '@lpg-manager/vehicle';
+import { TransporterModule } from '@lpg-manager/transporter';
 import { DispatchModule } from '@lpg-manager/dispatch';
-import { DriverModule } from '../../../../libs/backend/driver/src/lib/driver.module';
+import { DriverModule } from '@lpg-manager/driver';
 
 @Module({
   imports: [
